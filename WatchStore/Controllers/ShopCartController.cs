d1f6d8e6aff2a0e0cd4255408c337abad716fc15
Evgenii <evgenii.medvedskii@yandex.by>
2024-07-29 22:26:55 +0300

    Create not finished controller for cart. Some new little fixes

--- WatchStore/Controllers/ShopCartController.cs
+++ WatchStore/Controllers/ShopCartController.cs
@@ -1,6 +1,23 @@
-﻿namespace WatchStore.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using WatchStore.Data.Models;
+using WatchStore.Data.Repository;
+
+namespace WatchStore.Controllers
 {
     public class ShopCartController
     {
+        private readonly WatchesRepository? _watchesRepository;
+        private readonly StoreCart? _storeCart;
+
+        public ShopCartController(WatchesRepository watchesRepository, StoreCart storeCart)
+        {
+            _watchesRepository = watchesRepository;
+            _storeCart = storeCart;
+        }
+
+        //public ViewResult Index()
+        //{
+        //
+        //}
     }
 }
